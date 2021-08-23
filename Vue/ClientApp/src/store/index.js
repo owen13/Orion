ef855c7d48d0e0f453150f6b1ec613f2d3a8fc81
DESKTOP-KILB377\desktop <owen.mcgregor@gmail.com>
2021-08-23 16:22:15 +0100

    Vuex tutorial

--- Vue/ClientApp/src/store/index.js
+++ Vue/ClientApp/src/store/index.js
@@ -1,19 +1,23 @@
-import {createStore } from 'vuex'
+import { createStore } from 'vuex'
+import counterStore from './counter'
+
+import rootMutations from './mutations'
+import rootActions from './actions'
+import rootGetters from './getters'
 
 const store = createStore({
+    modules: {
+        numbers: counterStore
+    },
     state() {
         return {
-            counter: 0
+            counter: 0,
+            isLoggedIn: false
         }
     },
-    mutations: {
-        incrementCounter(state) {
-            state.counter++;
-        },
-        increaseCounter(state, payload) {
-            state.counter = state.counter + payload.value;
-        }
-    }
+    mutations: rootMutations,
+    actions: rootActions,
+    getters: rootGetters
 });
 
 export default store;
